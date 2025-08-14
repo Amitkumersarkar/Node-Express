bdbc0d9a6d935b07241c6e53199af5a40002b4bf
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-14 22:11:07 +0600

    added updated file

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,18 @@ function App() {
       <div>
         <h1>Users Management System</h1>
         <h3>Numbers of users : {users.length}</h3>
+        <form>
+          <input type="text" name="name" id="" />
+          <br />
+          <input type="email" name="email" id="" />
+          <br />
+          <input type="submit" value="add users" />
+        </form>
+        <div>
+          {
+            users.map((user) => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+          }
+        </div>
       </div>
     </>
   )
